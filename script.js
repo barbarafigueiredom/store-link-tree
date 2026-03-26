f595c70ebaa15d69afb3dde633be7a323919d1d3
Barbara Figueiredo <barbarafigueiredo74@gmail.com>
2026-03-26 13:35:10 -0300

    add project html and css

---
+++ script.js
@@ -0,0 +1,15 @@
+function toggleMode() {
+  const html = document.documentElement
+
+  //pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  // substituir a imagem
+  if (html.classList.toggle("light")) {
+    // se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/perfil_claro_redondo.png")
+  } else {
+    // se tiver sem light mode,manter a imagem normal
+    img.setAttribute("src", "./assets/perfil_escuro_redondo.png")
+  }
+}
